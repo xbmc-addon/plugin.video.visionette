7248eb29a8274c89b9aac8fe8024c973134f41ce
maxkhvorov <maxmang@gmail.com>
2014-12-25 11:02:57 +0300

    Fix maint`s scripts for Kodi

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`/../src
-cp -r ./* $HOME/Library/Application\ Support/XBMC/addons/plugin.video.visionette/
+cp -r ./* $HOME/Library/Application\ Support/Kodi/addons/plugin.video.visionette/
 
 rm -f $HOME/.xbmc/temp/xbmcup/plugin.video.visionette/visionette.sql
